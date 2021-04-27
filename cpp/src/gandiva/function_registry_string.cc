f021a1b41976e036bce9a4774651f55bfd56fc36
João Pedro <joaop@simbioseventures.com>
2021-04-27 12:33:07 -0300

    Add function registry for FROM_HEX function

--- cpp/src/gandiva/function_registry_string.cc
+++ cpp/src/gandiva/function_registry_string.cc
@@ -236,6 +236,9 @@ std::vector<NativeFunction> GetStringFunctionRegistry() {
       NativeFunction("binary_string", {}, DataTypeVector{utf8()}, binary(),
                      kResultNullIfNull, "binary_string", NativeFunction::kNeedsContext),
 
+      NativeFunction("from_hex", {}, DataTypeVector{utf8()}, binary(),
+                     kResultNullIfNull, "from_hex", NativeFunction::kNeedsContext),
+
       NativeFunction("split_part", {}, DataTypeVector{utf8(), utf8(), int32()}, utf8(),
                      kResultNullIfNull, "split_part",
                      NativeFunction::kNeedsContext | NativeFunction::kCanReturnErrors)};
