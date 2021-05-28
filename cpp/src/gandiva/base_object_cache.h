c16a368ff0709ae131677ef4b4509f7ae05670e0
Augusto Silva <augusto.a.silva@hotmail.com>
2021-05-28 14:10:34 -0300

    Refactor ProjectorObjectCache to BaseObjectCache with template

--- cpp/src/gandiva/base_object_cache.h
+++ cpp/src/gandiva/base_object_cache.h
@@ -7,25 +7,19 @@
 #include "gandiva/projector.h"
 
 namespace gandiva {
-
-class ProjectorObjectCache : public llvm::ObjectCache {
+template<class CacheKey>
+class BaseObjectCache : public llvm::ObjectCache {
  public:
-  //ProjectorObjectCache(SchemaPtr schema, const ExpressionVector& exprs,
-  //    SelectionVector::Mode selection_vector_mode,
-  //    std::shared_ptr<Configuration> configuration) :
-  //   projector_key_(schema, configuration, exprs, selection_vector_mode){
-  //  ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: Constructed the Projector Object Cache";
-  //}
-  ProjectorObjectCache(std::shared_ptr<Cache<ProjectorCacheKey, std::shared_ptr<llvm::MemoryBuffer>>>& cache,
-                       std::shared_ptr<ProjectorCacheKey>& key){
+  BaseObjectCache(std::shared_ptr<Cache<CacheKey, std::shared_ptr<llvm::MemoryBuffer>>>& cache,
+                       std::shared_ptr<CacheKey>& key){
     ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: Constructed the Projector Object Cache";
-    projector_cache_ = cache;
-    projector_key_ = key;
-    std::string cache_log = projector_cache_->toString();
+    cache_ = cache;
+    cache_key_ = key;
+    std::string cache_log = cache_->toString();
     ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: " << cache_log;
   };
 
-  ~ProjectorObjectCache() {
+  ~BaseObjectCache() {
     ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: Destructed the Projector Object Cache";
   }
 
@@ -33,12 +27,13 @@ class ProjectorObjectCache : public llvm::ObjectCache {
     ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: LLVM called notifyObjectCompiled() to compile the ObjectCode";
     std::unique_ptr<llvm::MemoryBuffer> obj_buffer = llvm::MemoryBuffer::getMemBufferCopy(Obj.getBuffer(), Obj.getBufferIdentifier());
     std::shared_ptr<llvm::MemoryBuffer> obj_code = std::move(obj_buffer);
-    projector_cache_->PutObjectCode(*projector_key_.get(), obj_code);
+    cache_->PutObjectCode(*cache_key_.get(), obj_code);
   };
 
   std::unique_ptr<llvm::MemoryBuffer> getObject(const llvm::Module* M){
     ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: LLVM called getObject() to get the cached ObjectCode";
-    std::shared_ptr<llvm::MemoryBuffer> cached_obj = projector_cache_->GetObjectCode(*projector_key_.get());
+    std::shared_ptr<llvm::MemoryBuffer> cached_obj =
+        cache_->GetObjectCode(*cache_key_.get());
     if(cached_obj == nullptr) {
       ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: LLVM cached ObjectCode was NOT found, need to compile it.";
       return nullptr;
@@ -49,13 +44,14 @@ class ProjectorObjectCache : public llvm::ObjectCache {
 
   };
 
-  void SetCache(std::shared_ptr<Cache<ProjectorCacheKey, std::shared_ptr<llvm::MemoryBuffer>>>&);
-  void SetKey(std::shared_ptr<ProjectorCacheKey>&);
-
-  std::shared_ptr<Cache<ProjectorCacheKey, std::shared_ptr<llvm::MemoryBuffer>>> GetCache();
-  std::shared_ptr<ProjectorCacheKey> GetKey();
+  std::shared_ptr<Cache<CacheKey, std::shared_ptr<llvm::MemoryBuffer>>> GetCache(){
+    return cache_;
+  }
+  std::shared_ptr<CacheKey> GetKey(){
+    return cache_key_;
+  }
  private:
-  std::shared_ptr<ProjectorCacheKey> projector_key_;
-  static std::shared_ptr<Cache<ProjectorCacheKey, std::shared_ptr<llvm::MemoryBuffer>>> projector_cache_;
+  std::shared_ptr<CacheKey> cache_key_;
+  std::shared_ptr<Cache<CacheKey, std::shared_ptr<llvm::MemoryBuffer>>> cache_;
 };
 }  // namespace gandiva
--- cpp/src/gandiva/engine.h
+++ cpp/src/gandiva/engine.h
@@ -22,14 +22,13 @@
 #include <string>
 #include <vector>
 
-#include "arrow/util/macros.h"
-
 #include "arrow/util/logging.h"
+#include "arrow/util/macros.h"
+#include "gandiva/base_object_cache.h"
 #include "gandiva/configuration.h"
 #include "gandiva/llvm_includes.h"
 #include "gandiva/llvm_types.h"
 #include "gandiva/visibility.h"
-#include "gandiva/projector_object_cache.h"
 
 namespace gandiva {
 
@@ -55,8 +54,18 @@ class GANDIVA_EXPORT Engine {
     functions_to_compile_.push_back(fname);
   }
 
-  /// Set ProjectorObjectCache.
-  Status SetProjectorObjectCache(ProjectorObjectCache& object_cache);
+  /// Set BaseObjectCache.
+  template<class KeyType>
+  Status SetLLVMObjectCache(BaseObjectCache<KeyType>& object_cache){
+    ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: Entered the SetLLVMObjectCache().";
+    execution_engine_->setObjectCache(&object_cache);
+    if (execution_engine_->hasError()){
+      return Status::ExecutionError("[OBJ-CACHE-LOG]: Can not set Projector Object cache");
+    } else {
+      ARROW_LOG(INFO) << "[OBJ-CACHE-LOG]: Exited with success the SetLLVMObjectCache().";
+      return Status::OK();
+    }
+  }
 
   /// Optimise and compile the module.
   Status FinalizeModule();
