b00e356846afa956ef7347497de951cb08bccd21
frank400 <j.victorhuguenin2018@gmail.com>
2021-08-09 21:42:31 -0300

    Fix return type of the macros

--- cpp/src/gandiva/precompiled/timestamp_arithmetic.cc
+++ cpp/src/gandiva/precompiled/timestamp_arithmetic.cc
@@ -210,18 +210,18 @@ TIMESTAMP_DIFF(timestamp)
 
 #define ADD_YEAR_MONTH_INTERVAL_TO_DATE_TYPES(TYPE)                              \
   FORCE_INLINE                                                                  \
-  gdv_timestamp add_year_month_interval_##TYPE(gdv_##TYPE millis,               \
+  gdv_##TYPE add_year_month_interval_##TYPE(gdv_##TYPE millis,               \
                                                gdv_year_month_interval count) { \
     EpochTimePoint tp(millis);                                                  \
-    return static_cast<gdv_timestamp>(tp.AddMonths(count).MillisSinceEpoch());  \
+    return static_cast<gdv_##TYPE>(tp.AddMonths(count).MillisSinceEpoch());  \
   }
 
 #define SUB_YEAR_MONTH_INTERVAL_TO_DATE_TYPES(TYPE)                                   \
   FORCE_INLINE                                                                       \
-  gdv_timestamp subtract_year_month_interval_##TYPE(gdv_##TYPE millis,               \
+  gdv_##TYPE subtract_year_month_interval_##TYPE(gdv_##TYPE millis,               \
                                                     gdv_year_month_interval count) { \
     EpochTimePoint tp(millis);                                                       \
-    return static_cast<gdv_timestamp>(tp.AddMonths(-1 * count).MillisSinceEpoch());  \
+    return static_cast<gdv_##TYPE>(tp.AddMonths(-1 * count).MillisSinceEpoch());  \
   }
 #define TIMESTAMP_ADD_INT32(TYPE)                                             \
   ADD_INT32_TO_TIMESTAMP_FIXED_UNITS(TYPE, timestampaddSecond, MILLIS_IN_SEC) \
